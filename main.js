6047f9b026bddfae677f4d737f1c9da03cc540c2
Alisson Melo <alissonmelo.work@gmail.com>
2021-08-23 03:01:18 -0300

    creates the testimonials and contacts section
    End of Class 03 - In Orbit

--- main.js
+++ main.js
@@ -29,3 +29,33 @@ window.addEventListener('scroll', event => {
     header.classList.remove('scroll')
   }
 })
+
+// Testimonials carrousel slider swiper
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+// SCROLLREVEAL
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links`,
+
+  { interval: 100 }
+)
